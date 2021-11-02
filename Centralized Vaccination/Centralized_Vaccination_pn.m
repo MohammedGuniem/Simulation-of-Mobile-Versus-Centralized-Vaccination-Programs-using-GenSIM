0a72d2fa61fa1d103fc320df9ce5ae55d222dbb3
Mohammed Zoher Guniem <2918474@uis.no>
2021-11-02 16:26:34 +0100

    Improving centralized vaccination

--- Centralized Vaccination/Centralized_Vaccination_pn.m
+++ Centralized Vaccination/Centralized_Vaccination_pn.m
@@ -1,11 +1,12 @@
 % MSF: Centeralized Vaccination Program
 
-clear all; 
+clear all;
 clc;
+
 
 global global_info
 global_info.START_AT = [8 0 0]; % Start simulations at 8:00 AM
-global_info.STOP_AT  = [15 0 0]; % Stop  simulations at 15:00
+global_info.STOP_AT  = [15 00 0]; % Stop  simulations at 15:00
 
 pns = pnstruct('Centralized_Vaccination_pn_pdf');
 
@@ -20,7 +21,10 @@ num_of_vaccines = num_of_residents
 
 dyn.m0 = {'p1',num_of_residents, 'p2',0, 'p3',0, 'p4',0, 'p5',num_of_vaccines};
 dyn.ft = {'tREGISTRATION',5, 'tVACCINATION',10, 'tWAITING',30};
-dyn.re = {'Staff',15,7*60,'Health_Workers',15,7*60,'Waiting_Rooms',30,7*60}; 
+dyn.re = {'Staff',12,7*60,'Health_Workers',24,7*60,'Waiting_Rooms',30,7*60}; 
+
+% optimal
+% dyn.re = {'Staff',12,7*60,'Health_Workers',24,7*60,'Waiting_Rooms',60,7*60}; 
 
 pni = initialdynamics(pns, dyn); 
 
---
+++ Centralized Vaccination/untitled.m
@@ -0,0 +1,8 @@
+clear all;
+clc;            
+
+if round(rand*10) > 5
+    disp(['1']);
+else
+    disp(['0']);
+end
